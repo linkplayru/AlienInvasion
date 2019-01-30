442add03150358dc30d4e22f8a58f986f126368d
Alexey Link <linkplay@yandex.ru>
2019-01-30 14:10:23 +0300

    add ship moving

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,11 +11,12 @@ def run_game():
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption(ai_settings.screen_caption)
 	
-	ship = Ship(screen)
+	ship = Ship(ai_settings, screen)
 	
 	#main cycle
 	while True:
-		gf.check_events()
+		gf.check_events(ship)
+		ship.update()
 		gf.update_screen(ai_settings, screen, ship)
 		
 run_game()
--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 class Settings():
 	
 	def __init__(self):
+		#screen
 		self.screen_caption = 'Alien Invasion'
-		self.screen_width = 1200
-		self.screen_height = 800
+		self.screen_width = 640
+		self.screen_height = 480
 		self.bg_color = (230, 230, 230)
+		
+		#ship
+		self.ship_speed_factor = 1.5
