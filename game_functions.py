d3ff1187a845eec924b172922d37ffabd15c7407
Alexey Link <linkplay@yandex.ru>
2019-01-31 13:27:00 +0300

    bullets now fatal

--- game_functions.py
+++ game_functions.py
@@ -81,11 +81,12 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 	aliens.draw(screen)
 	pygame.display.flip()
 	
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
 	bullets.update()
 	for bullet in bullets.copy():
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
+	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 			
 def update_aliens(ai_settings, aliens):
 	check_fleet_edges(ai_settings, aliens)
