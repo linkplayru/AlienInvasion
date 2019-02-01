c912e5afb3600b95f84457e3813204b600465884
Alexey Link <linkplay@yandex.ru>
2019-02-01 11:58:06 +0300

    hide mouse when game active

--- game_functions.py
+++ game_functions.py
@@ -20,6 +20,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 			
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
 	if play_button.rect.collidepoint(mouse_x, mouse_y) and not stats.game_active:
+		pygame.mouse.set_visible(False)
+		
 		stats.reset_stats()
 		stats.game_active = True
 		
@@ -124,6 +126,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 		sleep(0.5)
 	else:
 		stats.game_active = False
+		pygame.mouse.set_visible(True)
 			
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
 	screen_rect = screen.get_rect()
