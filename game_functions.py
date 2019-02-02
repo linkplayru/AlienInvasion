02bdbb35db13566ad5f8d35a5abb9b23963a53a9
linkplayru <linkplay@yandex.ru>
2019-02-02 17:50:16 +0300

    add lifes indicator

--- game_functions.py
+++ game_functions.py
@@ -29,6 +29,7 @@ def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens,
 		sb.prep_score()
 		sb.prep_high_score()
 		sb.prep_level()
+		sb.prep_ships()
 		
 		aliens.empty()
 		bullets.empty()
@@ -132,9 +133,10 @@ def check_bullet_alien_collision(ai_settings, screen, stats, sb, ship, aliens, b
 		sb.prep_level()
 		create_fleet(ai_settings, screen, ship, aliens)
 		
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
 	if stats.ships_left > 0:
 		stats.ships_left -= 1
+		sb.prep_ships()
 		aliens.empty()
 		bullets.empty()
 		create_fleet(ai_settings, screen, ship, aliens)
@@ -151,11 +153,11 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
 			ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 			break
 			
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
 	check_fleet_edges(ai_settings, aliens)
 	aliens.update()
 	if pygame.sprite.spritecollideany(ship, aliens):
-		ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+		ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
 	check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 	
 def check_high_score(stats, sb):
